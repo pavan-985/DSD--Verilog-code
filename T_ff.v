473ce7b6d9283f93cb95b9a8dd59f8b4dd905a99
Pavan_Agrawal <pavanagrawal698@ce.du.ac.in>
2025-11-17 16:21:58 +0530

    Add files via upload

---
+++ T_ff.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.11.2025 15:42:20
+// Design Name: 
+// Module Name: T_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module T_ff(
+    input T,clk,output reg Q,output Qbar
+    );
+    
+    assign Qbar=~Q;
+    initial Q = 0;
+    always @(posedge clk) begin
+        case(T)
+            1'b0: Q <= Q;       
+            1'b1: Q <= ~Q;    
+        endcase
+    end
+endmodule
